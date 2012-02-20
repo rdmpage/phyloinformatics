d4cf045418623e9c4bed2f5040cd684b024121fd
Roderic Page <rdmpage@gmail.com>
2012-02-20 18:02:13 +0000

    Improved display of gallery by generating square thumbnails

--- eoliphone/details.php
+++ eoliphone/details.php
@@ -2,6 +2,8 @@
 
 require_once('../lib.php');
 
+$max_images=24;
+
 $id = $_GET['id'];
 
 if ($id == 0)
@@ -16,19 +18,19 @@ if (isset($_GET['taxonID']))
 	$taxonID = $_GET['taxonID'];
 }
 
-$url = 'http://eol.org/api/pages/1.0/' . $id . '.json?details=1&common_names=1&images=10';
+$url = 'http://eol.org/api/pages/1.0/' . $id . '.json?details=1&common_names=1&images=' . $max_images;
 
 $json = get($url);
 $obj = json_decode($json);
 
 
-echo'
+$html = '
 <!DOCTYPE html> 
 <html> 
 	<head> 
 	<title></title> 
 	
-	<meta name="viewport" content="width=device-width; initial-scale=1.0; user-scalable=0;"> 
+	 <meta name="viewport" content="user-scalable=no, width=device-width,  initial-scale=1, minimum-scale=1,  maximum-scale=1"/>    
 	<meta name="apple-mobile-web-app-capable" content="yes" />
 
 	<link rel="stylesheet" href="../css/jquery.mobile-1.0.css" />
@@ -49,9 +51,9 @@ echo'
 	<script src="code.photoswipe.jquery-3.0.4.min.js"></script>
 	
 	<script type="text/javascript">
-		
-		//jQuery version
-			$(document).ready(function() {
+	
+		// Need to bind to this event so that PhotoSwipe loads		
+			$( "#details" ).live("pageinit", function() {
 				try {
 					var myPhotoSwipe = $(\'ul.gallery a\').photoSwipe({
 						enableMouseWheel : false,
@@ -61,7 +63,7 @@ echo'
 					console.log(\'unabled to load photoswipe\');
 				}
 			});
-		
+			
 	</script>
 	
 	
@@ -69,7 +71,7 @@ echo'
 
 <body>
 
-<div data-role="page">
+<div id="details" data-role="page" class="gallery-page">
 
 	<div data-role="header">
 		<a id="back" href="children.php?id=' . $_GET['taxonID'] . '" data-transition="slide" data-direction="reverse" data-icon="arrow-l">Back</a>
@@ -80,59 +82,69 @@ echo'
 	<div id="content" data-role="content" data-scroll="true">';
 	
 	
-	echo '<h1>' .  $obj->scientificName . '</h1>';
+	$html .= '<h1>' .  $obj->scientificName . '</h1>';
 	
 	
 	if (isset($obj->taxonConcepts))
 	{
-		echo '<h2>Classifications</h2>';
-		echo '<ul data-role="listview" data-inset="true">';
+		$html .= '<h2>Classifications</h2>';
+		$html .= '<ul data-role="listview" data-inset="true">';
 		foreach ($obj->taxonConcepts as $taxonConcept)
 		{
 			switch ($taxonConcept->nameAccordingTo)
 			{
 				case 'Species 2000 & ITIS Catalogue of Life: Annual Checklist 2010':
-					echo '<li><a href="children.php?id=' . $taxonConcept->identifier . '"><img class="ui-li-icon" src="images/catalogueoflife16x16.png"/>Catalogue of Life</a></li>';
+					$html .= '<li><a href="children.php?id=' . $taxonConcept->identifier . '"><img class="ui-li-icon" src="images/catalogueoflife16x16.png"/>Catalogue of Life</a></li>';
 					break;
 				case 'NCBI Taxonomy':
-					echo '<li><a href="children.php?id=' . $taxonConcept->identifier . '"><img class="ui-li-icon"  src="images/ncbi16x16.png"/>NCBI Taxonomy</a></li>';
+					$html .= '<li><a href="children.php?id=' . $taxonConcept->identifier . '"><img class="ui-li-icon"  src="images/ncbi16x16.png"/>NCBI Taxonomy</a></li>';
 					break;
 				case 'WORMS Species Information (Marine Species)':
-					echo '<li><a href="children.php?id=' . $taxonConcept->identifier . '"><img class="ui-li-icon"  src="images/worms16x16.png"/>World Register of Marine Species</a></li>';
+					$html .= '<li><a href="children.php?id=' . $taxonConcept->identifier . '"><img class="ui-li-icon"  src="images/worms16x16.png"/>World Register of Marine Species</a></li>';
 					break;
 				default:
 					break;
 			}
 		}
-		echo '</ul>';
+		$html .= '</ul>';
 	}
 	
 	
 	if (isset($obj->dataObjects))
 	{
-		echo '<h2>Images</h2>';
+		$html .= '<h2>Images</h2>';
 		
-		echo '<ul class="gallery" id="Gallery">';
+		$html .= '<ul class="gallery" id="Gallery">';
 		foreach ($obj->dataObjects as $dataObject)
 		{
 			if (isset($dataObject->eolThumbnailURL))
 			{
-				echo '<li>';
-				echo '<a href="' . $dataObject->eolMediaURL . '" rel="external">';
-				echo '<img src="' . $dataObject->eolThumbnailURL . '"></img>';
-				echo '</a>';
-				echo '</li>';
+				$caption = '';
+				if (isset($dataObject->title))
+				{
+					$caption = $dataObject->title;
+				}
+			
+			
+				$html .= '<li>';
+				$html .= '<a href="' . $dataObject->eolMediaURL . '" rel="external">';
+				$html .= '<img src="eolthumbnail.php?url=' . urlencode($dataObject->eolThumbnailURL) . '" alt = "' . $caption . '"></img>';
+				$html .= '</a>';
+				$html .= '</li>';
 			}
 		}
-		echo '</ul>';
+		$html .= '</ul>';
 	}
 	
-echo '
+$html .= '
 	</div><!-- /content -->
 
 </div><!-- /page -->
 
 </body>
 </html>';
+
+header("Content-type: text/html");
+echo $html;
 
 ?>
---
+++ eoliphone/eolthumbnail.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once (dirname(dirname(__FILE__)) . '/lib.php');
+
+$convert = true;
+
+$thumbnail_size = 128;
+
+$url = $_GET['url'];
+
+$hash = md5($url);
+
+$filename = 'tmp/' . $hash .  '.jpg';
+
+if (!file_exists($filename))
+{
+	$image = $url;
+
+	// fetch and store				
+	$img = get($image);
+	
+	if ($convert)
+	{
+		file_put_contents('tmp/' . $hash, $img);
+		$command = "/usr/local/bin/convert -thumbnail '" . $thumbnail_size . "x" . $thumbnail_size . "^' -gravity center -extent " . $thumbnail_size . "x" . $thumbnail_size . " " . 'tmp/' . $hash . ' ' . $filename;
+		system($command);
+	}
+	else
+	{
+		file_put_contents($filename, $img);
+	}
+}
+
+if (file_exists($filename))
+{
+	$img = file_get_contents($filename);
+	header("Content-type: image/jpeg");
+	echo $img;
+}
+else
+{
+	$img = file_get_contents('images/80x80.png');
+	header("Content-type: image/png");
+	echo $img;
+}
+
+
+?>
